0d7c2e0c8d00099e537a6c73cc87304ded1a0ec7
ssribeirolmmp <sergio@lmmp.mec.puc-rio.br>
2020-06-24 11:52:35 -0300

    LegendFLMassComments

--- main.py
+++ main.py
@@ -8,21 +8,39 @@ from matplotlib import pyplot as plt
 # df = pd.read_csv(".Labeled/20150429.xlsx") 
 
 # Read Experiment Dataframe
-df = pd.read_excel(r'./Labeled/20151124.xlsx', sheet_name='Data')
+df = pd.read_excel(r'./Labeled/20151005.xlsx', sheet_name='Data')
 df['DATETIME'] = pd.to_datetime(df[['YEAR','MONTH','DAY','HOUR','MINUTE','SECOND']])
 
+# Sensor Tag
+sensorTag = 'P_6C'
+fluidLossMass = 'FL_M_2A'
 
-sensorTag = 'P_3B'
-presure_psi = df.set_index('DATETIME')[sensorTag]
+# psi to Pa Conversion Factor
+psi2Pa = 6894.76
+
+# Zone of Interest
 df = df.set_index(pd.DatetimeIndex(df['DATETIME']))
-df = df[(df['PASSED_SECONDS']<=20000)]
-filter = (df['MINUTE']%10==0)
-fig,ax1 = plt.subplots()
-df[sensorTag].plot(ax = ax1)
+df = df[(df['PASSED_SECONDS']<=22500)]
+df = df[(df['PASSED_SECONDS']>=3000)]
+fig,ax = plt.subplots(1,2)
 
+# Pressure(psi)
+presure_psi = df.set_index('DATETIME')[sensorTag]
+presure_psi.plot(ax = ax[0],label=sensorTag)
+
+# Resampled Pressure(psi)
+filter = (df['MINUTE']%10==0)
 groups = df[filter].groupby(['YEAR','MONTH','DAY','HOUR','MINUTE']).first().set_index('DATETIME')
-groups[sensorTag].plot(ax=ax1)
+groups[sensorTag].plot(ax=ax[0],label=sensorTag+'_resampled')
 print(groups.iloc[0:10])
+
+# Resampled Pressure(g)
+groups[fluidLossMass].plot(ax=ax[1], label=fluidLossMass,color='red')
+
+# Show Plot
+leg1 = ax[0].legend()
+leg2 = ax[1].legend()
+
 plt.show()
 a=1
 # Define Time Series for each pressure sensor\
